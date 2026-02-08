83cd4db98191a15d41e4ce11ee7832945e07ab1e
SPACETAN <andrii.podolskyi@gmail.com>
2026-02-07 16:06:22 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nechto_runtime",
+    version="0.0.0",
+    packages=find_packages(),
+    description="Minimal deterministic NECHTO runtime",
+)
